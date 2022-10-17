43b7d432d102c714ffddba2212f567e842a5bc31
aydinnemati <aydinnemati199888@gmail.com>
2022-10-17 15:40:12 +0330

    testing

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -7,13 +7,13 @@ import logging
 #     time.sleep(5)
 #     logging.info("CreateUser has been finished!")
 
-def sub_domains_enumeration(arg):
-    logging.info("Key: " + str(arg.key) + "; Value: " + str(arg.value))
+def sub_domains_enumeration(args):
+    logging.info(str(args))
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     # variable = sdk.job(name, description, function, [dependencies, ...])
     # migratedb = sdk.Job("DB Migration", "Imports newest test data dump and migrates to newest version.", MigrateDB, ["Create DB User"])
-    sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration({"target": "aydin.com"}))
+    sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration("aydin.com"))
     sdk.serve([sub_domains_enumeration_call])
