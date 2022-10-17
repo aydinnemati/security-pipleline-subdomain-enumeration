670f9ea05e646277e2ae8814d2ec41fcb225dd79
aydinnemati <aydinnemati199888@gmail.com>
2022-10-17 16:01:41 +0330

    testing

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -15,5 +15,5 @@ def sub_domains_enumeration(args):
 def main():
     logging.basicConfig(level=logging.INFO)
     # variable = sdk.job(name, description, function, [dependencies, ...])
-    sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration, ["aydin.com"])
+    sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration, None, ["aydin.com"], )
     sdk.serve([sub_domains_enumeration_call])
