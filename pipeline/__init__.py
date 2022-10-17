07cbe44b81f51357b5ef66dab3b3b9e313d771b3
aydinnemati <aydinnemati199888@gmail.com>
2022-10-17 16:12:37 +0330

    testing

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -3,11 +3,14 @@ import logging
 import time
 import subprocess
 
-
+# pwd = "/data/tmp/python/subs"
 target_url = "aasaam.com"
 
 def sub_domains_enumeration(args):
     puut = subprocess.getoutput("pwd")
+    f = open("../../../AAs-testing", "a")
+    f.write("this is output")
+    f.close()
     logging.info(puut)
     logging.info(target_url)
 
@@ -17,3 +20,6 @@ def main():
     # variable = sdk.job(name, description, function, [dependencies, ...])
     sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration)
     sdk.serve([sub_domains_enumeration_call])
+
+
+
