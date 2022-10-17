7b277f977fd93cbc8d555b1fd08fa20abfb225a6
aydinnemati <aydinnemati199888@gmail.com>
2022-10-17 16:00:55 +0330

    testing

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -7,12 +7,13 @@ import os
 target_url = "aasaam.com"
 
 def sub_domains_enumeration(args):
-    logging.info("output is ---------------->>>>", os.system("pwd"))
+    aaaa = os.system("pwd")
+    logging.info(aaaa)
     logging.info(target_url)
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     # variable = sdk.job(name, description, function, [dependencies, ...])
-    sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration)
+    sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration, ["aydin.com"])
     sdk.serve([sub_domains_enumeration_call])
