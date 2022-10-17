8d4fd7db8dcd15a9804f423fa0966fdfe217af4b
aydinnemati <aydinnemati199888@gmail.com>
2022-10-17 15:06:46 +0330

    testing

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -2,42 +2,12 @@ from gaiasdk import sdk
 import logging
 import time
 
-def CreateUser(args):
-    logging.info("CreateUser has been started!")
-    time.sleep(5)
-    logging.info("CreateUser has been finished!")
+# def CreateUser(args):
+#     logging.info("CreateUser has been started!")
+#     time.sleep(5)
+#     logging.info("CreateUser has been finished!")
 
-def MigrateDB(args):
-    logging.info("MigrateDB has been started!")
-    time.sleep(5)
-    logging.info("MigrateDB has been finished!")
-
-def CreateNamespace(args):
-    logging.info("CreateNamespace has been started!")
-    time.sleep(5)
-    logging.info("CreateNamespace has been finished!")
-
-def CreateDeployment(args):
-    logging.info("CreateDeployment has been started!")
-    time.sleep(5)
-    logging.info("CreateDeployment has been finished!")
-
-def CreateService(args):
-    logging.info("CreateService has been started!")
-    time.sleep(5)
-    logging.info("CreateService has been finished!")
-
-def CreateIngress(args):
-    logging.info("CreateIngress has been started!")
-    time.sleep(5)
-    logging.info("CreateIngress has been finished!")
-
-def Cleanup(args):
-    logging.info("Cleanup has been started!")
-    time.sleep(5)
-    logging.info("Cleanup has been finished!")
-
-def Subdomainenumeretaion(args):
+def sub_domains_enumeration(args):
     logging.info(args)
 
 
@@ -45,5 +15,5 @@ def main():
     logging.basicConfig(level=logging.INFO)
     # variable = sdk.job(name, description, function, [dependencies, ...])
     # migratedb = sdk.Job("DB Migration", "Imports newest test data dump and migrates to newest version.", MigrateDB, ["Create DB User"])
-    Subdomainenumeretaioncall = sdk.Job("testingggggggggggggs...........", "desciriptions are useless", Subdomainenumeretaion("gooooooooooooooooooog.go"))
-    sdk.serve([Subdomainenumeretaioncall])
+    sub_domains_enumeration_call = sdk.Job("MY TEST JOB NAME IS SUNDOMAINS ENUMERATION", "desciriptions are useless", sub_domains_enumeration("gooooooooooooooooooog.go"))
+    sdk.serve([sub_domains_enumeration_call])
